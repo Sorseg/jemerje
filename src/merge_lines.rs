0e7096149c26d4b8fcabd61e9657b94339b07e1b
demoth <bubnov.d.e@gmail.com>
2024-10-04 11:12:13 +0200

    add merge lines

---
+++ src/merge_lines.rs
@@ -0,0 +1,36 @@
+pub fn sweet_merge_line() -> &'static [&'static str] {
+    &[
+        "26_chocolate.png",
+        "30_chocolatecake.png",
+        "34_donut.png",
+        "52_gingerbreadman.png",
+        "46_fruitcake.png",
+        "57_icecream.png",
+        "59_jelly.png",
+        "61_jam.png",
+        "50_giantgummybear.png",
+        "90_strawberrycake.png",
+        "79_pancakes.png",
+    ]
+}
+
+pub fn salty_merge_line() -> &'static [&'static str] {
+    &[
+        "15_burger.png",
+        "32_curry.png",
+        "38_friedegg.png",
+        "92_sandwich.png",
+        "44_frenchfries.png",
+        "69_meatball.png",
+        "54_hotdog.png",
+        "81_pizza.png",
+        "85_roastedchicken.png",
+        "99_taco.png",
+        "95_steak.png"
+    ]
+}
+
+pub enum MergeLine {
+    SWEET,
+    SALTY,
+}
